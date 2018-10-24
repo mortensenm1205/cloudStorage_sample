e4ca60baac9b07d2ea645a432813a6edd7a856e9
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-10-24 16:46:30 -0600

    bringing in persist

--- src/config/store.js
+++ src/config/store.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import storage from 'redux-persist/lib/storage';
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import { combineReducers } from "redux";
+import { persistStore, persistReducer } from 'redux-persist';
 import { imageData } from "../components/Display/reducers";
 import { imageUploadData } from '../components/Upload/reducers';
 
@@ -10,7 +12,19 @@ const rReducer = combineReducers({
   imageUploadData
 });
 
-export const store = createStore(
-  rReducer,
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const store = createStore(
+  persistReducer(persistConfig, rReducer),
   compose(applyMiddleware(thunk, logger))
 );
+
+const persistor = persistStore(store);
+
+export {
+  store,
+  persistor
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import { store } from "./config/store";
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from "./config/store";
 import Root from "./screens/Root";
 
 const rootElement = document.getElementById("root");
 render(
   <Provider store={store}>
-    <Root />
+    <PersistGate loading={null} persistor={persistor}>
+      <Root />
+    </PersistGate>
   </Provider>,
   rootElement
 );
