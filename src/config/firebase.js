61b06911dff409ed9d3192e7caa5d4958105a699
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-10-24 14:50:05 -0600

    initial. updating gitignore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/storage";
 
 let config = {
-  apiKey: "AIzaSyDz4I4AF_PX2u8nEeKR--npwQ4wdLVk3lg",
-  authDomain: "first-cloud-storage-28749.firebaseapp.com",
-  databaseURL: "https://first-cloud-storage-28749.firebaseio.com",
-  projectId: "first-cloud-storage-28749",
-  storageBucket: "first-cloud-storage-28749.appspot.com",
-  messagingSenderId: "397100267399"
+    apiKey: "AIzaSyDz4I4AF_PX2u8nEeKR--npwQ4wdLVk3lg",
+    authDomain: "first-cloud-storage-28749.firebaseapp.com",
+    databaseURL: "https://first-cloud-storage-28749.firebaseio.com",
+    projectId: "first-cloud-storage-28749",
+    storageBucket: "first-cloud-storage-28749.appspot.com",
+    messagingSenderId: "397100267399"
 };
 
 firebase.initializeApp(config);
